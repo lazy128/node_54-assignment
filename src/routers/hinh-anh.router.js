290d986c9a8aa628accff0abf3a0b2b8c1725a13
lazy128 <duy.nguyenduy128@hcmut.edu.vn>
2026-04-23 18:35:20 +0700

    the latest updated

--- src/routers/hinh-anh.router.js
+++ src/routers/hinh-anh.router.js
@@ -7,15 +7,18 @@ const hinhAnhRouter = express.Router();
 
 hinhAnhRouter.get("/", hinhAnhController.getDanhSachAnh);
 hinhAnhRouter.get("/tim-kiem", hinhAnhController.timKiemAnh);
-hinhAnhRouter.post("/upload", protect, uploadMemoryStorage.single("file_anh"), hinhAnhController.uploadAnh);
+
+// FIX LỖI 404: Đổi từ /upload thành / để khớp với imageService.create của Frontend
+// Nếu bạn dùng FormData để gửi ảnh, hãy đảm bảo Frontend gửi key là "file_anh"
+hinhAnhRouter.post("/", protect, uploadMemoryStorage.single("file_anh"), hinhAnhController.uploadAnh);
 
 hinhAnhRouter.get("/:id", protect, hinhAnhController.getThongTinAnh);
 hinhAnhRouter.get("/:id/binh-luan", protect, hinhAnhController.getBinhLuan);
 hinhAnhRouter.get("/:id/da-luu", protect, hinhAnhController.kiemTraDaLuu);
 hinhAnhRouter.post("/:id/binh-luan", protect, hinhAnhController.postBinhLuan);
-// hinhAnhRouter.post("/", protect, hinhAnhController.themAnh);
 hinhAnhRouter.delete("/:id", protect, hinhAnhController.xoaAnh);
 hinhAnhRouter.post("/:id/luu-anh", protect, hinhAnhController.luuAnh);
 hinhAnhRouter.delete("/:id/luu-anh", protect, hinhAnhController.xoaLuuAnh);
 hinhAnhRouter.put("/:id", protect, hinhAnhController.capNhatAnh);
+
 export default hinhAnhRouter;
